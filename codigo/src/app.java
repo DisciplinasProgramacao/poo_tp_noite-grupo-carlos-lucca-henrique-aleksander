b35f4b9d6b8d0831cef440fe5b998028a6eca602
Aleksander Ribeiro Vale <aleksander.ribeiro@sga.pucminas.br>
2023-04-22 21:36:33 -0300

    Alterações da segunda sprint

---
+++ codigo/src/app.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.io.FileNotFoundException;
+
+
+public class app {
+    public static void main(String[] args) throws FileNotFoundException {
+        Streaming teste = new Streaming();
+        teste.iniciar();
+    }
+}
